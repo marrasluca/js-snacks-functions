6a0bfdf2c0d45dda22a75b997383f1c00c140f3e
marrasluca <lmarras10@gmail.com>
2025-01-16 18:24:21 +0100

    add: 04

--- snack-funzioni/04_conta_vocali/script.js
+++ snack-funzioni/04_conta_vocali/script.js
@@ -4,10 +4,13 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function contaVocali(str) {
+    const vocali = ['a', 'e', 'i', 'o', 'u'];
+    return str.split('').filter(carattere => vocali.includes(carattere.toLowerCase())).length;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
-
+console.log(contaVocali(word));
 
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
